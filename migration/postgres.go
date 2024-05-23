c4bf06d175becafb6f1daea381f208bb24afe6a6
dimasyudhana <dimas.yudhana@gmail.com>
2024-05-23 23:07:33 +0700

    6th commit

--- migration/postgres.go
+++ migration/postgres.go
@@ -13,6 +13,7 @@ import (
 	"github.com/golang-migrate/migrate"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
 )
 
 type migrator struct {
